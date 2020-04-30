a990947f8b5847e42de8857419d6a8fd9660426b
codingcruz <moises@asistecnia.app>
2020-04-30 02:42:27 -0400

    Commit Inicial

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+console.log(miNuevoNombre);
+
+// alert('Hola desde app.js');
+
+// console.log('Hola Mundo');
+
+let a = 10,
+  b = 20,
+  c = "Hola ",
+  d = "Spiderman",
+  x = a + b;
+
+const SALUDO = c + d;
+
+// console.log('%c Mis Variables', 'color: Yellow; font-weight: bold');
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+c = "Hola denuevo";
+
+// console.table({ a, b, c, d, x });
+
+// let miNuevoNombre = 'Moisés Cruz';
---
+++ assets/js/primitivos.js
@@ -0,0 +1,28 @@
+let nombre = 'Peter Parker'; // String
+console.log(nombre);
+
+nombre = 'Ben Parker';
+console.log(nombre);
+
+nombre = 'Tía May';
+nombre = `Tìa May`;
+
+console.log(typeof nombre);
+
+nombre = 123; // Tipo cambia a number
+console.log(typeof nombre);
+
+let esMarvel = false;
+console.log(typeof esMarvel);
+
+let edad = 33;
+console.log(typeof edad);
+
+edad = 33.001; //Sigue siendo un número
+console.log(typeof edad);
+
+let superPoder; //Null
+console.log(typeof superPoder);
+
+let soyNull = null; //Object
+console.log(typeof superPoder);
